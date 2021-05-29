5768bd154d8015776a43779af2d1ec98d0ad57c8
juanbaldassin <juanbaldassin86@icloud.com>
2021-05-30 00:05:01 +0200

    initial commit

---
+++ page_objects/login_page.py
@@ -0,0 +1,56 @@
+class LoginPage():
+
+
+    USERNAMAE_INPUT = "un"
+    PASSWORD_INPUT = "pw"
+    SINGIN_BTN = "btn primary"
+    POST_HERE = "gwt-uid-9"
+    ACTION_BTN = "SiteMenuTab_TEMPO_SITE---nav_label"
+    EXHIBICION_LINK = "aui-ActionLink GPGIXADDAP"
+    NOMBRE_EXHIBICION = "de8a7a6c750895ffa0554040ea436941"
+    TEMA_EXHIBICION = "147b7d8997ae86d5138581b25ac8c77f"
+
+
+    def __init__(self, driver):
+        self.driver = driver
+
+
+
+    def login_in_app(self, username, password):
+        username_input = self.driver.find_element_by_id(self.USERNAME_INPUT)
+        username_input.send_keys(username)
+
+        password_input = self.driver.find_element_by_id(self.PASSWORD_INPUT)
+        password_input.send_keys(password)
+
+        singin_btn = self.driver.find_element_by_class(self.SINGIN_BTN)
+        singin_btn.click()
+
+
+
+
+
+
+    def is_singin_succesfull(self):
+        post_here = self.driver.find_element_by_id(self.POST_HERE).is_displayed()
+        return post_here
+
+
+    def action(self):
+        action_btn = self.driver.find_element_by_class(self.ACTION_BTN)
+        action_btn.click()
+
+        exhibicion_link = self.driver.find_element_by_class(self.EXHIBICION_LINK)
+        exhibicion_link.click()
+
+    def fill_the_fields(self, nombre_exhibicion, tema_exhibicion):
+
+        nombre_exhibicon_input = self.driver.find_element_by_id(self.NOMBRE_EXHIBICION)
+        nombre_exhibicon_input.send_keys(nombre_exhibicion)
+
+        tema_exhibicion_input = self.driver.find_element_by_id(self.TEMA_EXHIBICION)
+        tema_exhibicion_input.send_keys(tema_exhibicion)
+
+
+
+
---
+++ test_cases/test_login.py
@@ -0,0 +1,43 @@
+import unittest
+import HtmlTestRunner
+from ddt import ddt, file_data
+from selenium import webdriver
+import sys
+import os
+
+sys.path.append("//home//PycharmProjects//ceita_task")
+
+from page_objects.login_page import LoginPage
+
+
+@ddt
+class Testlogin(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe")
+        cls.driver.get("https://ceita-dev.appiancloud.com/suit/")
+        cls.driver.maximize_window()
+        cls.login_page = LoginPage(driver = cls.driver)
+
+    @file_data(os.path.join("data", "test_login.json"))
+    def test_login(self, username, password):
+        self.login_page.login_in_app(username=username, password=password)
+        is_login_succesful = self.login_page.is_singin_succesfull()
+
+        assert is_login_succesful is True
+
+
+    @file_data(os.path.join("data", "fill_the_field.json"))
+    def fill_the_field(self, nombre_de_la_exhibicion, tema_de_la_exhibicion):
+        self.login_page.fill_the_fields(nombre_exhibicion=nombre_de_la_exhibicion, tema_exhibicion=tema_de_la_exhibicion)
+
+
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output="//home//PycharmProjects//ceita_task//reports"))
